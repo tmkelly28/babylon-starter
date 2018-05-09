70837f106b3462c90a4386d00af8ec920b3e28df
Thomas Kelly <tmkelly28@gmail.com>
2018-05-09 12:22:05 -0400

    extract from namespace

--- app/index.js
+++ app/index.js
@@ -1,29 +1,36 @@
-import * as BABYLON from 'babylonjs'
+import {
+  Engine,
+  Scene,
+  FreeCamera,
+  Vector3,
+  HemisphericLight,
+  Mesh
+} from 'babylonjs'
 
 // Get the canvas DOM element
 const canvas = document.getElementById('renderCanvas')
 
 // Load the 3D engine
-const engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true})
+const engine = new Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true})
 
 // CreateScene function that creates and return the scene
 const createScene = () => {
   // Create a basic BJS Scene object
-  const scene = new BABYLON.Scene(engine)
+  const scene = new Scene(engine)
   // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}
-  const camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene)
+  const camera = new FreeCamera('camera1', new Vector3(0, 5, -10), scene)
   // Target the camera to scene origin
-  camera.setTarget(BABYLON.Vector3.Zero())
+  camera.setTarget(Vector3.Zero())
   // Attach the camera to the canvas
   camera.attachControl(canvas, false)
   // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
-  const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene)
+  const light = new HemisphericLight('light1', new Vector3(0, 1, 0), scene)
   // Create a built-in "sphere" shape its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
-  const sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene, false, BABYLON.Mesh.FRONTSIDE)
+  const sphere = Mesh.CreateSphere('sphere1', 16, 2, scene, false, Mesh.FRONTSIDE)
   // Move the sphere upward 1/2 of its height
   sphere.position.y = 1
   // Create a built-in "ground" shape its constructor takes 6 params : name, width, height, subdivision, scene, updatable
-  const ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false)
+  const ground = Mesh.CreateGround('ground1', 6, 6, 2, scene, false)
   // Return the created scene
   return scene
 }
